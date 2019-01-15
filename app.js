dd77b2d7780a011c1ec58f1e8095d7c73d5aba4a
dmullen17 <dmullen17@gmail.com>
2019-01-15 14:22:21 -0800

    project 1 JS

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+/*
+bands.sort((a, b) => a < b ? -1 : 1);
+*/
 const bandsList = document.querySelector('ul[class="bands"]');
+
+//let input = 'The Plot in you'
+function removeArticles(input) {
+    const articles = ['(the)', '(a )', '(in )', '(as )', '(an )', '(and )', '(, )'];
+    const regex = new RegExp(articles.join('|'), 'gi');
+    return input.replace(regex, ' ').
+                                    trim().
+                                    replace(/\s\s+/g, ' ');
+}
+//console.log(removeArticles(input));
+
+bands.sort((firstBand, secondBand) => {
+    return removeArticles(firstBand) < removeArticles(secondBand) ? -1 : 1; 
+})
 
 function populateList(bands = [], bandsList) {
     bandsList.innerHTML = bands.map(band => {
